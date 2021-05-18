3dd6cbbac884d2b697c3f15bd9572b83db0874dc
NeehaRampilla <70782003+NeehaRampilla@users.noreply.github.com>
2021-05-17 18:44:11 -0700

    Add files via upload

--- Food.js
+++ Food.js
@@ -40,4 +40,17 @@ class Food {
         }
       }
     }
+    bedroom(){
+      background(bedroom,550,500);
+
+    }
+    garden(){
+      background(garden,550,500);
+      
+    }
+    washroom(){
+      background(washroom,550,500);
+      
+    }
+    
 }
